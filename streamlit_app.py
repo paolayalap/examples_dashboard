8b565c6fa964794add71443a05f9f05f24d0ea3c
paolayalap <99574202+paolayalap@users.noreply.github.com>
2025-10-18 11:56:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,19 @@
 import streamlit as st
 import pandas as pd #para implementar tablas
 
+st.title("Título grande")
+st.header("Encabezado intermedio")
+st.subheader("Subtítulo")
+st.text("Texto plano (sin formato)")
+st.caption("Texto tipo nota o pie de página")
 st.write('Hi everyone!')
+st.markdown("*Texto en cursiva*")
+st.markdown("**Texto en negrita**")
+st.markdown("***Texto en cursiva y negrita***")
+st.markdown("Este es un **texto importante** con *palabras en cursiva* dentro.")
+st.markdown("# Título H1")
+st.markdown("## Título H2")
+st.markdown("### Título H3")
 
 # ------ Subir un CSV y mostrarlo como tabla -------
 #st.title("Cargar CSV y mostrar tabla")
@@ -44,5 +56,5 @@ st.button(
 )
 
 if st.button("Procesar datos"):
-    st.success("¡Listo!")
+    st.success("¡Listo!", type="secundary")
 
