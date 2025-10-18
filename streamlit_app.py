3767c5af1328d74f7d968c0f0f9ae00ab5c3807c
paolayalap <99574202+paolayalap@users.noreply.github.com>
2025-10-18 09:59:33 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,6 @@ df = pd.DataFrame({
     "pH": [7.1, 7.4, 7.0, 7.3],
     "Conductividad (µS/cm)": [420, 510, 385, 460]
 })
-
 editado = st.data_editor(
     df,
     use_container_width=True,
@@ -29,29 +28,4 @@ st.write(editado)
 
 
 
-
-"""st.title("Tabla interactiva")
-df = pd.DataFrame({
-    "Fecha": pd.date_range("2025-10-01", periods=6, freq="D"),
-    "Chl-a (µg/L)": [12.1, 18.7, 33.2, 41.8, 22.0, 15.4],
-    "Ficocianina (µg/L)": [3.2, 4.0, 5.1, 7.3, 4.2, 3.8]
-})
-
-st.dataframe(
-    df,
-    use_container_width=True,   # ocupa todo el ancho
-    height=320,                 # alto del contenedor con scroll
-)"""
-
-
-
-""""st.title("Tabla estática")
-data = {
-    "Sensor": ["pH", "Turbidez", "DO", "Conductividad", "Temperatura"],
-    "Valor": [7.2, 12.5, 6.8, 420.0, 24.3],
-    "Unidad": ["pH", "NTU", "mg/L", "µS/cm", "°C"]
-}
-df = pd.DataFrame(data)
-
-st.table(df)  # Render plano (no scroll, no ordenar)"""
 
