a4403a397997db51de502ab256a57f0d9f374ad5
paolayalap <99574202+paolayalap@users.noreply.github.com>
2025-10-18 09:54:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
+#Librerías
 import streamlit as st
+import pandas as pd #para implementar tablas
 
-st.title('🎈 App Name')
+st.title('Examples')
+st.write('Hi everyone!')
 
-st.write('Hello world!')
+st.title("Tabla estática")
+data = {
+    "Sensor": ["pH", "Turbidez", "DO", "Conductividad", "Temperatura"],
+    "Valor": [7.2, 12.5, 6.8, 420.0, 24.3],
+    "Unidad": ["pH", "NTU", "mg/L", "µS/cm", "°C"]
+}
+df = pd.DataFrame(data)
+
+st.table(df)  # Render plano (no scroll, no ordenar)
+
