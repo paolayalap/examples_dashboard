dee2a9d0837050fd64cffc915b77a28d6799287f
paolayalap <99574202+paolayalap@users.noreply.github.com>
2025-10-18 11:40:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,26 @@ st.write('Hi everyone!')
 #    st.success(f"Filas: {df.shape[0]} | Columnas: {df.shape[1]}")
 #    st.dataframe(df, use_container_width=True)
 
-# ------- Botón --------
+# ------- Botón con color personalizado --------
+#st.markdown("""
+#<style>
+#div.stButton > button {
+#    background-color: #ff4b4b;   /* rojo personalizado */
+#    color: white;
+#    border-radius: 12px;
+#    padding: 0.6em 1.2em;
+#    font-weight: bold;
+#    border: none;
+#}
+#div.stButton > button:hover {
+#    background-color: #ff7878;   /* color al pasar el mouse 
+#    transform: scale(1.03);
+#}
+#</style>
+#""", unsafe_allow_html=True)
+#st.button("Botón rojo personalizado")
+
+# ------- Botón simple --------
 def correr_modelo(k):
     st.write(f"Entrenando modelo con k={k}...")
 
@@ -23,23 +42,16 @@ st.button(
     on_click=correr_modelo,
     kwargs={"k": 5},
 )
-st.markdown("""
-<style>
-div.stButton > button {
-    background-color: #ff4b4b;   /* rojo personalizado */
-    color: white;
-    border-radius: 12px;
-    padding: 0.6em 1.2em;
-    font-weight: bold;
-    border: none;
-}
-div.stButton > button:hover {
-    background-color: #ff7878;   /* color al pasar el mouse */
-    transform: scale(1.03);
-}
-</style>
-""", unsafe_allow_html=True)
 
-st.button("Botón rojo personalizado")
+def boton1(i):
+    st.write(f"Número de veces que se ha presionado el botón: {i}")
+st.button(
+    "Numeración",
+    type="secundary",
+    help ="", #esta ayuda aparece cuando se pasa el cursor por encima del botón
+    on_click=boton1,
+    
+)
+
 
 
